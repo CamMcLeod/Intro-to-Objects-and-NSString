0975b784b4a7320324a3693fbd838bddff7b5bb1
Cam McLeod <cammcleod3@gmail.com>
2019-05-15 15:43:06 -0700

    completion of Madlib task

--- Strings/main.m
+++ Strings/main.m
@@ -23,6 +23,16 @@ int main(int argc, const char * argv[]) {
         NSString *nameReplacing = [name stringByReplacingOccurrencesOfString: name withString: nameUppercase];
        
         NSLog(@"Name in Uppercase: %@\n Name in Lowercaser: %@\n Name Appending: %@\n Name Replacing with Uppercase: %@\n", nameUppercase, nameLowercase, nameAppending, nameReplacing);
+        
+        NSString *madLib = @"Yesterday, _PERSON_ and I when to the park. On our way to the _ADJECTIVE_1 park, we saw a _ADJECTIVE_2 _NOUN_ on a bike. We also saw big _ADJECTIVE_2 balloons tied to the _NOUN_. Once we got to the _ADJECTIVE_1 park, the sky turned _ADJECTIVE_2. It started to _VERB_. _PERSON_ and I _VERB_ all the way home.";
+        
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_PERSON_" withString:@"Jonny Law"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_1" withString:@"grimy"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_2" withString:@"plush"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_NOUN_" withString:@"raccoon"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_VERB_" withString:@"scream"];
+        
+        NSLog(@"%@", madLib);
     }
     return 0;
 }
